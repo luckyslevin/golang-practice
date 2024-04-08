a4f446dc7daeaaf2d43239e8fab7cc000348fcb1
Albert Cueme <atcueme@vauldex.com>
2024-04-08 10:28:41 +0800

    Add

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.JSON(fiber.Map{"hello": "world"})
 	})
 
 	app.Listen(":3000")
